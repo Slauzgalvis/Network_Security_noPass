5cbc620bd5c28cc6d5135d16a6ec8f0bde519a1c
Azas64 <deiviskuokalt@gmail.com>
2018-04-29 13:32:48 +0300

    cookies

--- Main.sh
+++ Main.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+cd Phyton
+python3 get_cookies.py
+cp cookies.txt ../Patterns/cookies.txt
+cd ..
 cd Wish_lists
+chmod +x Wish_lists.sh
 ./Wish_lists.sh
 cd ..
 cd Charts
+chmod +x Charts.sh
 ./Charts.sh
 cd ..
 cd Users
+chmod +x Users.sh
 ./Users.sh
 cd ..
---
+++ Phyton/get_cookies.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+auth_username = 'tesotest22@gmail.com'
+auth_password = 'super1234'
+
+LOGIN_URL = 'http://www.fxwebsolution.com//demo/arthi/multivendor/sign-in.php'
+SELENIUM_PATH = '/usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs'
+driver = webdriver.PhantomJS(executable_path=SELENIUM_PATH)
+
+def login_session():
+    driver.get(LOGIN_URL)
+    username = driver.find_element_by_id('exampleInputEmail1')
+    password = driver.find_element_by_id('exampleInputPassword1')
+    username.send_keys(auth_username)
+    password.send_keys(auth_password)
+    driver.find_element_by_name('loginuser').click()
+
+login_session()
+
+cookies = driver.get_cookies()[0]
+f = open("cookies.txt","w+")
+f.write('{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(str(cookies['domain']), str(cookies['httponly']), str(cookies['path']), str(cookies['secure']), '0', str(cookies['name']), str(cookies['value'])))
+f.close()
